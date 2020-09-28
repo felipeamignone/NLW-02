41d0f93dc6ae022d8b46d46533c5a62a6491c45e
Felipe Mignone <dev.felipemignone@gmail.com>
2020-09-28 02:13:23 -0300

    asd

--- src/routs.tsx
+++ src/routs.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import {BrowserRouter, Route} from 'react-router-dom'
 
+import Form from './pages/Form';
 import Landing from './pages/Landing';
 import TeacherForm from './pages/TeacherForm';
 import TeacherList from './pages/TeacherList';
